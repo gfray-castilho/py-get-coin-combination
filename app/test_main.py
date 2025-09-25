03c0ca8226f21b668b742f2ca1f43152550a9557
gfray-castilho <gfraycastilho@gmail.com>
2025-09-25 15:47:16 -0300

    solved tasks

--- app/test_main.py
+++ app/test_main.py
@@ -6,28 +6,29 @@ from app.main import get_coin_combination
     "cents, expected",
     [
         (0, [0, 0, 0, 0]),
-        (1 , [1, 0, 0, 0]),
-        (6 , [1, 1, 0, 0]),
-        (17 , [2, 1, 1, 0]),
-        (50 , [0, 0, 0, 2]),
-
-    ]
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+    ],
 )
-def test_get_coin_combination(cents, expected):
-    assert get_coin_combination(cents) is expected
-    assert isinstance (get_coin_combination(cents), list)
-    assert len(expected) == 4
+def test_get_coin_combination(cents: int, expected: list) -> None:
+    result = get_coin_combination(cents)
+    assert result == expected
+    assert isinstance(result, list)
+    assert len(result) == 4
 
 
-@pytest.mark.parametrize("bad_input",
-                         [
-                             3.5,
-                             "10",
-                             None,
-                             [1],
-                             {"word": 10}
-                         ]
-                         )
-def test_get_coin_combination_errors(bad_input: object):
+@pytest.mark.parametrize(
+    "bad_input",
+    [
+        3.5,
+        "10",
+        None,
+        [1],
+        {"word": 10},
+    ],
+)
+def test_get_coin_combination_errors(bad_input: object) -> None:
     with pytest.raises(TypeError):
         get_coin_combination(bad_input)
